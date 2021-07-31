53fea0aacad957c661fe32699d4cde6778ece32b
doni <donihamster88@gmail.com>
2021-08-01 00:19:57 +0700

    update database

--- admin/application/config/database.php
+++ admin/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'tokocodi_tokopulsa',
+	'hostname' => 'localhost',
 	'username' => 'tokocodi_tokopulsa',
 	'password' => 'qwerty888@',
-	'database' => 'tokopulsa',
+	'database' => 'tokocodi_tokopulsa',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
